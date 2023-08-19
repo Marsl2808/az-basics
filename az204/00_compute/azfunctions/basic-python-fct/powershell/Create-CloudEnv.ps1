1a90161b3a9ca9bf0947f6fc49a18d3dfc822a3c
Marsl <m.wendling88@gmx.de>
2023-08-19 16:05:48 +0200

    intermediate commit

--- az204/00_compute/azfunctions/basic-python-fct/powershell/Create-CloudEnv.ps1
+++ az204/00_compute/azfunctions/basic-python-fct/powershell/Create-CloudEnv.ps1
@@ -11,5 +11,12 @@ New-AzResourceGroup -Name $GROUP_NAME -Location $LOCATION
 "---- create sa ----"
 New-AzStorageAccount -ResourceGroupName $GROUP_NAME -Name $STORAGE_NAME -SkuName Standard_LRS -Location $LOCATION
 
-"---- create function app ----"
-New-AzFunctionApp -ResourceGroupName $GROUP_NAME -Name $APP_NAME -StorageAccountName $STORAGE_NAME -FunctionsVersion 4 -OSType linux -RuntimeVersion 3.9 -Runtime python -Location $LOCATION
+# optional: create premium- ("EP1") or dedicated ("B1") plan
+# comment following to create (default) consumption plan
+#"---- create plan ----"
+#$PLAN_SKU="B1" #"B1"
+#$PLAN_NAME="fct-plan"
+#New-AzFunctionAppPlan -Name $PLAN_NAME -ResourceGroupName $GROUP_NAME -Location $LOCATION -Sku $PLAN_SKU -WorkerType Linux
+
+#"---- create function app ----"
+New-AzFunctionApp -ResourceGroupName $GROUP_NAME -Name $APP_NAME -StorageAccountName $STORAGE_NAME -FunctionsVersion 4 -OSType Linux -RuntimeVersion 3.9 -Runtime python -Location $LOCATION
--- az204/00_compute/azfunctions/basic-python-fct/powershell/Delete-CloudEnv.ps1
+++ az204/00_compute/azfunctions/basic-python-fct/powershell/Delete-CloudEnv.ps1
@@ -1,4 +1,7 @@
 $GROUP_NAME='fct-rg'
+$PLAN_NAME="fct-plan"
+
+#Remove-AzAppServicePlan -Name $PLAN_NAME -ResourceGroupName $GROUP_NAME
 
 #az group delete --name $GROUP_NAME -y
 Remove-AzResourceGroup -Name $GROUP_NAME
